a433db66e6994ea29f2f9296231bf9a5c0bf902c
hexafluoride <xafluoride@gmail.com>
2022-05-17 16:17:59 -0500

    Fix crosschain payload serialization

--- PactSharp/Types/PactPayload.cs
+++ PactSharp/Types/PactPayload.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace PactSharp.Types;
 
 public class PactPayload
 {
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public PactExecPayload? Exec { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public PactContPayload? Cont { get; set; }
 }
